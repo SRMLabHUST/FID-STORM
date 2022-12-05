2417a2795cdce423a4914f48413de41f6224026e
zzw <1258806185@qq.com>
2022-12-05 12:25:25 +0800

    FID-STORM build

--- Java/src/dll.java
+++
@@ -1,6 +0,0 @@
-
-
-public class dll {
-    // 传入参数
-    public native int Initialization(String jinputDataDir,String joutputDataDir,double jbatchSize,double jmodelType,double jAmplification,boolean jfp);
-}
